1e8af8a356a11a0d6fa170b3051f48ad57035f27
Devedu <deveduar@gmail.com>
2024-07-18 18:49:39 +0200

    app init, dark mode, buttons and dropdown

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button"
+import { buttonVariants } from "@/components/ui/button"
+import Link from "next/link";
+import { ModeToggle } from "@/components/theme-toogle"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+    <main className="flex min-h-screen flex-col items-center justify-between p-7 relative">
+
+    <div className="bg-slate-200 z-10 font-mono text-sm  space-y-3 flex flex-col p-4 absolute top-10 left-10">
+      <Button variant="outline">Button</Button>
+      <Link className={buttonVariants({ variant: "outline" })} href="/">Click here</Link>
+      <Button variant="outline">Button</Button>
+      <Link className={buttonVariants({ variant: "outline" })} href="/">Click here</Link>
+      <Link className={buttonVariants({ variant: "outline" })} href="/">Click here</Link>
+      <ModeToggle/>
+    </div>
+
+    <div className="bg-slate-200 z-10 font-mono text-sm  space-x-3 flex flex-row p-4 m-7 absolute top-0 right-10">
+      <ModeToggle/>
+      <ModeToggle/>
+    </div>
+
+
+
+
+      {/* <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/app/page.tsx</code>
@@ -107,7 +129,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
-      </div>
+      </div> */}
     </main>
   );
 }
